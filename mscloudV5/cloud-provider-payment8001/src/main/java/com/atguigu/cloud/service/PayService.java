bb2d7969b8bf238ff249f771ac22ae77653a1c2a
action <xingdong1117@126.com>
2024-10-30 17:21:04 +0800

    feat:pay模块编码落地

--- mscloudV5/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/service/PayService.java
+++ mscloudV5/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/service/PayService.java
@@ -12,10 +12,6 @@ public interface PayService {
     public int add(Pay pay);
     public int delete(Integer id);
     public int update(Pay pay);
-
     public Pay getById(Integer id);
-
     public List<Pay> getAll();
-
-
 }
