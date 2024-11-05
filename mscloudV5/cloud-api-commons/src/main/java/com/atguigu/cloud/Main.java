f76ffb40ec8e0a00ec09b8ff6357245e3cf8eacb
action <xingdong1117@126.com>
2024-11-05 09:06:33 +0800

    feat:cloud-api-commons 模块

---
+++ mscloudV5/cloud-api-commons/src/main/java/com/atguigu/cloud/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu.cloud;
+
+/**
+ * @author: action
+ * @create: 2024/11/4 17:37
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
