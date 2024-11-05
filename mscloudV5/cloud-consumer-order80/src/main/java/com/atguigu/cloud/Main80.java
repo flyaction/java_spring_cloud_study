bfb48267d79030bc43e0cf2c5fd965e418040419
action <xingdong1117@126.com>
2024-11-05 15:09:42 +0800

    feat:consul服务注册与发现

--- mscloudV5/cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
+++ mscloudV5/cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
@@ -2,12 +2,14 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: action
  * @create: 2024/11/4 09:57
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Main80 {
     public static void main(String[] args) {
         SpringApplication.run(Main80.class,args);
