6c044c78480deb2025fa277664c27255305e5111
action <xingdong1117@126.com>
2024-11-19 15:10:56 +0800

    fix:链路监控test

---
+++ mscloudV5/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayMicrometerController.java
@@ -0,0 +1,24 @@
+package com.atguigu.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: action
+ * @create: 2024/11/19 15:02
+ **/
+@RestController
+public class PayMicrometerController {
+    /**
+     * Micrometer(Sleuth)进行链路监控的例子
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/pay/micrometer/{id}")
+    public String myMicrometer(@PathVariable("id") Integer id)
+    {
+        return "Hello, 欢迎到来myMicrometer inputId:  "+id+" \t    服务返回:" + IdUtil.simpleUUID();
+    }
+}
