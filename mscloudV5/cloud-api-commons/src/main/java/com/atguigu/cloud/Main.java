a0cd9bab7d25cf8645e82b2b828030b5a1ba3062
action <xingdong1117@126.com>
2024-11-21 11:19:10 +0800

    fix:Predicate-after+before+between

--- mscloudV5/cloud-api-commons/src/main/java/com/atguigu/cloud/Main.java
+++ mscloudV5/cloud-api-commons/src/main/java/com/atguigu/cloud/Main.java
@@ -1,11 +1,14 @@
 package com.atguigu.cloud;
 
+import java.time.ZonedDateTime;
+
 /**
  * @author: action
  * @create: 2024/11/4 17:37
  **/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ZonedDateTime zdt = ZonedDateTime.now();//默认时区
+        System.out.println(zdt);
     }
 }
