1557963b59a2bce4fedd00caa0236600f7e1e676
action <xingdong1117@126.com>
2024-11-28 16:32:59 +0800

    feat:sentinel热点规则

--- mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
+++ mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
@@ -5,6 +5,7 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -53,4 +54,18 @@ public class RateLimitController
         log.error("程序逻辑异常了:{}", e);
         return "程序逻辑异常了"+"\t"+e.getMessage();
     }
+
+    // 热点规则
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+    public String dealHandler_testHotKey(String p1,String p2,BlockException exception)
+    {
+        return "-----dealHandler_testHotKey";
+    }
+
 }
