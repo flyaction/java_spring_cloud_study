69925724f3f4b6f2593b3731be58bfd51b2ad8ae
action <xingdong1117@126.com>
2024-11-28 16:13:02 +0800

    feat:sentinel按照资源名称限流+自定义限流返回+服务降低处理

--- mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
+++ mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +30,27 @@ public class RateLimitController
     public String handleException(BlockException exception)
     {
         return "服务不可用@SentinelResource启动"+"\t"+"o(╥﹏╥)o";
+    }
+
+
+
+    @GetMapping("/rateLimit/doAction/{p1}")
+    @SentinelResource(value = "doActionSentinelResource",
+            blockHandler = "doActionBlockHandler", fallback = "doActionFallback")
+    public String doAction(@PathVariable("p1") Integer p1) {
+        if (p1 == 0){
+            throw new RuntimeException("p1等于零直接异常");
+        }
+        return "doAction";
+    }
+
+    public String doActionBlockHandler(@PathVariable("p1") Integer p1,BlockException e){
+        log.error("sentinel配置自定义限流了:{}", e);
+        return "sentinel配置自定义限流了";
+    }
+
+    public String doActionFallback(@PathVariable("p1") Integer p1, Throwable e){
+        log.error("程序逻辑异常了:{}", e);
+        return "程序逻辑异常了"+"\t"+e.getMessage();
     }
 }
