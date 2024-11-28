d19574bc9162e433ab448b325ed78156c31d6003
action <xingdong1117@126.com>
2024-11-28 10:07:24 +0800

    feat:sentinel熔断规则-慢调用比例

--- mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
+++ mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -5,6 +5,8 @@ import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author: action
  * @create: 2024/11/27 10:56
@@ -49,4 +51,18 @@ public class FlowLimitController
         System.out.println(System.currentTimeMillis()+"      testE,排队等待");
         return "------testE";
     }
+
+    /**
+     * 新增熔断规则-慢调用比例
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF()
+    {
+        //暂停几秒钟线程
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("----测试:新增熔断规则-慢调用比例 ");
+        return "------testF 新增熔断规则-慢调用比例";
+    }
+
 }
