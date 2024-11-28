998b87c1fa7e338bc1ad3909565c4f53c6124e75
action <xingdong1117@126.com>
2024-11-28 15:53:07 +0800

    feat:sentinel按照资源名称限流

--- mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
+++ mscloudV5/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,16 @@ public class RateLimitController
     public String byUrl()
     {
         return "按rest地址限流测试OK";
+    }
+
+    @GetMapping("/rateLimit/byResource")
+    @SentinelResource(value = "byResourceSentinelResource",blockHandler = "handleException")
+    public String byResource()
+    {
+        return "按资源名称SentinelResource限流测试OK";
+    }
+    public String handleException(BlockException exception)
+    {
+        return "服务不可用@SentinelResource启动"+"\t"+"o(╥﹏╥)o";
     }
 }
